9c8ac805617404051b630c8153a1e37a045da1d6
唐湘 <tangxiang@tangxiangdeMacBook-Pro.local>
2021-05-29 17:07:13 +0800

    v1.1.0

--- bootstrap.js
+++ bootstrap.js
@@ -5,12 +5,12 @@ const os = require("os");
 const ip = require("ip");
 const open = require("open");
 const serveIndex = require("./serve-index");
+const config = require(path.join(os.homedir(), "./.swsrc.json"));
 
 const app = express();
 const address = ip.address();
-const port = 80;
-const public = path.join(os.homedir(), "./共享");
-const config = require(path.join(os.homedir(), "./.swsrc.json"));
+const port = config.port;
+const public = path.join(os.homedir(), config.public);
 
 app.use(
   basicAuth({
